fcdb10957e14a797d6440b2efa917c87d6a0f7a4
Techcellent360 <isholaazeez40@gmail.com>
2021-08-04 18:06:47 +0100

    update

--- votingapp/settings/base.py
+++ votingapp/settings/base.py
@@ -165,7 +165,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+ 
 # STATIC_URL = '/static/'
 
 # MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
@@ -177,7 +177,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "static", "staticfiles")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media","mediafiles") 
+MEDIA_ROOT = os.path.join(BASE_DIR, "livemedia","media-root") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
